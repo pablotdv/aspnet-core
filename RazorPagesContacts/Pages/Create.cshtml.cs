f594dc372b77554c0bdeb62d4939c2ab11a95fcc
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-12-20 13:53:36 -0200

    First commit

---
+++ RazorPagesContacts/Pages/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesContacts.Data;
+
+namespace RazorPagesContacts.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public CreateModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [TempData]
+        public string Message { get; set; }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Customers.Add(Customer);
+            await _db.SaveChangesAsync();
+            Message = $"Customer {Customer.Name} added";
+            return RedirectToPage("/Index");
+        }
+    }
+}
