24fb313c798d391644a27a2c0f83fb37c3188d81
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-12-21 13:14:29 -0200

    Criar uma API Web com o ASP.NET Core

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -40,5 +40,56 @@ namespace TodoApi.Controllers
 
             return new ObjectResult(item);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(long id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = await _context.TodoItems.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.IsComplete = item.IsComplete;
+            todo.Name = item.Name;
+
+            _context.TodoItems.Update(todo);
+            await _context.SaveChangesAsync();
+
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var todo = await _context.TodoItems.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todo);
+            await _context.SaveChangesAsync();
+            return new NoContentResult();
+        }
     }
 }
